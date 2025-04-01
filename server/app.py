d5473972c00cb476dc3575454333aec42cbd47a0
Edmund <edmundgachanja@gmail.com>
2025-04-01 12:26:39 +0300

    lab

--- server/app.py
+++ server/app.py
@@ -18,26 +18,60 @@ class ClearSession(Resource):
 class Signup(Resource):
     
     def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username']
-        )
-        user.password_hash = json['password']
-        db.session.add(user)
+        user = request.get_json()
+
+        username = user["username"]
+        password = user["password"]
+
+        if not username and not password:
+            return {"error": "422: Unprocessable Entity"}, 422
+        
+        new_user = User(username=username)
+        new_user.password_hash = password
+
+        db.session.add(new_user)
         db.session.commit()
-        return user.to_dict(), 201
+
+        session["user_id"] = new_user.id
+
+        return new_user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session.get("user_id")).first()
+        
+        if not user:
+            return {},204
+        return user.to_dict(), 201
 
 class Login(Resource):
-    pass
+
+    def post(self):
+
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': '401 Unauthorized'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session["user_id"] = None
+        return {},204
+
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
